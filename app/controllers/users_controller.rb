faf127af065d2a7e86f9fdb2b158c4246da750c4
hisatetsu <76477593+hisatetsu@users.noreply.github.com>
2024-06-01 08:20:03 +0000

    Finish user edit ,update,index,and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
 
   def show
     @user = User.find(params[:id])
-
   end
   
   def new
@@ -44,6 +44,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    #HTTPステータスコード「303 See Other」
+    redirect_to users_url, status: :see_other
+  end
+
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password,
@@ -66,5 +74,11 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       #redirect_to(root_url, status: :see_other) unless @user == current_user
       redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
     end    
+    
 end
